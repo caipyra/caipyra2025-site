34472d0ac59a8a960da6ca5a59c9aa02d6fb7404
Danilo Martins <mawkee@gmail.com>
2025-06-09 11:44:58 -0300

    Adicionado link para ingressos de tutoriais, e link extra para palestras

--- src/components/Programacao.jsx
+++ src/components/Programacao.jsx
@@ -1,5 +1,3 @@
-import EmailLink from './EmailLink';
-
 export default function Programacao() {
   return (
     <>
@@ -9,11 +7,46 @@ export default function Programacao() {
             <div className="col-md-12 col-md-offset-1 section-title">
               <h2>Programação</h2>
             </div>
+            <div className="row">
+              <div className="col-xs-6 col-md-6">
+                <i className="fa fa-chalkboard"></i>
+                <h3>19/06 - Tutoriais</h3>
+                <a
+                  href="https://caipyra2025.eventbrite.com.br"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button type="button" className="btn btn-custom btn-lg">
+                    Ingresso gratuito para tutoriais <i className="fas fa-ticket"></i>
+                  </button>
+                </a>
+              </div>
+              <div className="col-xs-6 col-md-6">
+                <i className="fa fa-podcast"></i>
+                <h3>20/06 e 21/06 - Palestras</h3>
+                <a
+                  href="https://caipyra2025.eventbrite.com.br"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button type="button" className="btn btn-custom btn-lg">
+                    Ingressos à venda - 1º Lote <i className="fas fa-ticket"></i>
+                  </button>
+                </a>
+              </div>
+              {/* <div className="col-xs-6 col-md-4">
+                <i className="fa fa-rocket"></i>
+                <h3>22/06 - Sprints</h3>
+                <p>Atividades de sprints a serem definidas em breve</p>
+              </div> */}
+            </div>
+            <div className="row" style={{ marginTop: '60px' }}>
               <pretalx-schedule
                 event-url="https://talks.python.org.br/caipyra-2025/"
                 locale="en"
                 style={{ '--pretalx-clr-primary': '#0d6efd' }}
               />
+            </div>
           </div>
         </div>
       </div>
