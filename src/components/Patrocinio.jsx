25fd74b08715bdf174f84f101f9c0cb032c76e18
Danilo Martins <mawkee@gmail.com>
2025-06-16 13:01:26 -0300

    Modificado logo da NIC.br

--- src/components/Patrocinio.jsx
+++ src/components/Patrocinio.jsx
@@ -1,5 +1,4 @@
-import cgi_br from '/img/patrocinadores/cgi-br.png';
-import nic_br from '/img/patrocinadores/nic-br.png';
+import nic from '/img/patrocinadores/nic-cgi.png';
 import psf from '/img/patrocinadores/psf.png';
 import apyb from '/img/patrocinadores/apyb.png';
 import fatec from '/img/patrocinadores/fatec.png';
@@ -35,19 +34,14 @@ export default function Patrocinio() {
                 </a>
               </div>
               <div className="col-6 col-md-4 mb-3 mb-md-0">
-                <a href="https://www.cgi.br/" target="_blank" rel="noopener noreferrer">
-                  <img src={cgi_br} alt="CGI.br" className="img-fluid" />
-                </a>
-              </div>
-              <div className="col-6 col-md-4 mb-3 mb-md-0">
                 <a href="https://www.nic.br/" target="_blank" rel="noopener noreferrer">
-                  <img src={nic_br} alt="NIC.br" className="img-fluid" />
+                  <img src={nic} alt="NIC.br" className="img-fluid" />
                 </a>
               </div>
             </div>
             <h3>Apoio</h3>
             <div className="row align-items-center justify-content-center patrocinadores-row">
-              <div className="col-6 col-md-6 mb-3 mb-md-0">
+              <div className="col-6 col-md-4 mb-3 mb-md-0">
                 <a
                   href="http://www.fatecrp.edu.br"
                   target="_blank"
@@ -56,7 +50,7 @@ export default function Patrocinio() {
                   <img src={fatec} alt="FATEC" className="img-fluid" />
                 </a>
               </div>
-              <div className="col-12 col-md-6">
+              <div className="col-12 col-md-4">
                 <a
                   href="https://apyb.python.org.br"
                   target="_blank"
